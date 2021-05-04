3ef38b2ce2e679c0389df2dba8705fd429b4c193
Marton <martonpss@gmail.com>
2021-05-04 11:42:02 -0300

    Change icons

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ let showPointsEnabled = true;
 let showLinesEnabled = true;
 let showCurvesEnabled = true;
 
+let firstAction = false;
+
 displayButtons();
 
 
@@ -12,36 +14,39 @@ function displayButtons() {
   options.innerHTML = '';
 
   if (!createNewEnabled)
-    addButton('create-new', 'Create new', 'fas fa-plus', 'createNew()');
+    addButton('create-new', 'Create new', 'far fa-plus', 'createNew()');
   else
-    addButton('done', 'Done', 'fas fa-check', 'done()');
+    addButton('done', 'Done', 'far fa-check', 'done()');
 
-  addButton('clear-all', 'Clear all', 'fas fa-redo', 'clearAll()');
+  addButton('clear-all', 'Clear all', 'far fa-redo', 'clearAll()');
 
   if (!showPointsEnabled)
-    addButton('show-points', 'Show points', 'fas fa-circle', 'toggleShowPoints()');
+    addButton('show-points', 'Show points', 'far fa-circle', 'toggleShowPoints()');
   else
-    addButton('show-points', 'Hide points', 'fas fa-circle', 'toggleShowPoints()');
+    addButton('show-points', 'Hide points', 'far fa-circle', 'toggleShowPoints()', true);
 
   if (!showLinesEnabled)
-    addButton('show-lines', 'Show lines', 'fas fa-grip-lines', 'toggleShowLines()');
+    addButton('show-lines', 'Show lines', 'far fa-horizontal-rule', 'toggleShowLines()');
   else
-    addButton('show-lines', 'Hide lines', 'fas fa-grip-lines', 'toggleShowLines()');
+    addButton('show-lines', 'Hide lines', 'far fa-horizontal-rule', 'toggleShowLines()', true);
 
   if (!showCurvesEnabled)
-    addButton('show-curves', 'Show curves', 'fab fa-confluence', 'toggleShowCurves()');
+    addButton('show-curves', 'Show curves', 'far fa-wave-sine', 'toggleShowCurves()');
   else
-    addButton('show-curves', 'Hide curves', 'fab fa-confluence', 'toggleShowCurves()');  
+    addButton('show-curves', 'Hide curves', 'far fa-wave-sine', 'toggleShowCurves()', true);  
 }
 
-function addButton(id, text, icon, func) {
+function addButton(id, text, icon, func, checked = false) {
   const buttonContainer = document.createElement('div');
   buttonContainer.setAttribute('class', 'button-container');
   buttonContainer.setAttribute('id', id);
 
+  const toHide = checked ? '<i class="far fa-slash" style="font-size: 150%"></i>' : '';
+
   buttonContainer.innerHTML = `
-    <button onclick="${func}">
-      <i class="${icon}"></i>
+    <button onclick="${func}" style="position: relative">
+      <i class="${icon}" style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);"></i>
+      ${toHide}
     </button>
     <div class="button-description">
       <p>${text}</p>
@@ -54,10 +59,14 @@ function addButton(id, text, icon, func) {
 function createNew() {
   createNewEnabled = true;
   displayButtons();
-  resizeCanvasElement();
-
+  
   let cnv = document.getElementById('canvas');
   cnv.style.cursor = "crosshair";
+
+  if (!firstAction) {
+    resizeCanvasElement();
+    firstAction = true;
+  }
 }
 
 function clearAll() {
