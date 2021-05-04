05416fc0c31fa942ec6cd91bb84294803030d771
Marton <martonpss@gmail.com>
2021-05-04 16:34:14 -0300

    Remove unused function

--- sketch.js
+++ sketch.js
@@ -53,20 +53,6 @@ function draw() {
   }
 }
 
-function drawArrow(base, vec, myColor) {
-  push();
-  stroke(myColor);
-  strokeWeight(3);
-  fill(myColor);
-  translate(base.x, base.y);
-  line(0, 0, vec.x, vec.y);
-  rotate(vec.heading());
-  let arrowSize = 7;
-  translate(vec.mag() - arrowSize, 0);
-  triangle(0, arrowSize / 2, 0, -arrowSize / 2, arrowSize, 0);
-  pop();
-}
-
 function showPoints() {
   stroke('#EC904C');
   strokeWeight(10);
